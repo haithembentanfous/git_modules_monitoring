6d70e37a0501c0623891ad6d2724b322d4b4e3a8
haithembentanfous <htanfous@gmail.com>
2022-03-22 14:44:47 +0100

    update workspace

--- git_log.py
+++ git_log.py
@@ -3,15 +3,22 @@ from os import path
 import shutil
 
 
+#TODO make it an args
+WORKSPACE_PATH="git_test"
+
 #clone repo
+if path.exists(WORKSPACE_PATH) == True:
+    shutil.rmtree(WORKSPACE_PATH)
 
-if path.exists("/media/haithem/DATA/Haithem/works/test") == True:
-    shutil.rmtree("/media/haithem/DATA/Haithem/works/test")
-
-git.Repo.clone_from("https://github.com/haithembentanfous/generate_html_from_json.git","/media/haithem/DATA/Haithem/works/test")
+git.Repo.clone_from("https://github.com/haithembentanfous/generate_html_from_json.git",WORKSPACE_PATH)
 
 
 g = git.Git("/media/haithem/DATA/Haithem/works/test")
 loginfo = g.log('--format=LOGS|%an-%ad-%s','--name-only')
 
 print(loginfo)
+
+
+#cleanup
+if path.exists(WORKSPACE_PATH) == True:
+    shutil.rmtree(WORKSPACE_PATH)
